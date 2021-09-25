c2976d768a31938fa22ddf9d54dde4a267a175b4
JamesThePaladin <Jamesthepaladin@gmail.com>
2021-09-25 12:07:35 -0700

    Added Intro Scene Functionality
    Added a script that moves from the intro scene after a set amount of seconds

---
+++ Assets/Scripts/Scene/SceneMover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMover : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)]
+    private float timer = 3f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(SceneChangeTimer());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Attack")) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    //A timer for switching scenes from the intro scene
+    IEnumerator SceneChangeTimer()
+    {
+        yield return new WaitForSeconds(timer);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
